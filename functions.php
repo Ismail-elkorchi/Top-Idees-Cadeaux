29b3379a4200ed9022be7ad55f2c5d0172338891
Ismail El Korchi <ismail.elkorchi@gmail.com>
2019-04-10 10:43:16 +0100

    Change the Moroccan dirham currency symbol

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Change the Moroccan dirham currency symbol
+ */
+add_filter('woocommerce_currency_symbol', 'tic_moroccan_currency_symbol', 10, 2);
+
+function tic_moroccan_currency_symbol( $currency_symbol, $currency ) {
+     switch( $currency ) {
+          case 'MAD': $currency_symbol = ' MAD'; break;
+     }
+     return $currency_symbol;
+}
