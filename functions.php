9d40aa41544ceca5e45cd14f0442e44e8fdbb214
Ismail El Korchi <ismail.elkorchi@gmail.com>
2019-04-12 23:56:20 +0100

    Move the main menu to the right of the logo

--- functions.php
+++ functions.php
@@ -18,3 +18,24 @@ function tic_moroccan_currency_symbol( $currency_symbol, $currency ) {
      }
      return $currency_symbol;
 }
+
+/**
+ * Remove storefront actions
+ */
+add_action( 'init', 'tic_remove_sf_actions' );
+
+function tic_remove_sf_actions() {
+
+     remove_action( 'storefront_header', 'storefront_product_search', 40 );
+     remove_action('homepage', 'storefront_recent_products', 30 );
+
+
+     remove_action( 'storefront_header', 'storefront_primary_navigation_wrapper', 42 );
+     remove_action( 'storefront_header', 'storefront_primary_navigation_wrapper_close', 68 );
+
+	remove_action( 'storefront_header', 'storefront_primary_navigation', 50 );
+     add_action( 'storefront_header', 'storefront_primary_navigation', 30 );
+     remove_action( 'storefront_header', 'storefront_header_cart', 60 );
+     add_action( 'storefront_header', 'storefront_header_cart', 30 );
+
+}
